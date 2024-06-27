788dd00ad6a437c7ab34fe603dec315e83a2b232
jorgealamar <jorge_alamar@live.com>
2024-06-26 23:19:27 -0300

    confirmar

---
+++ app/(modals)/login.tsx
@@ -0,0 +1,127 @@
+import Colors from '@/constants/Colors';
+import { defaultStyles } from '@/constants/Styles';
+import { useWarmUpBrowser } from '@/hooks/useWarmUpBrowser';
+import { useOAuth } from '@clerk/clerk-expo';
+import { Ionicons } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
+import React from 'react';
+import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+
+enum Strategy{
+  Apple = 'oauth_apple',
+  Google = 'oauth_google',
+  Facebook = 'oauth_facebook',
+}
+
+const Page = () => {
+  useWarmUpBrowser();
+  const router = useRouter ()
+  const {startOAuthFlow: appleAuth} = useOAuth({strategy:'oauth_apple'});
+  const {startOAuthFlow: googleAuth} = useOAuth({strategy:'oauth_google'})
+  const {startOAuthFlow: facebookAuth} = useOAuth({strategy:'oauth_facebook'});
+
+  const onSelectAuth = async (strategy: Strategy) => {
+    const selectedAuth = {
+      [Strategy.Google]: googleAuth,
+      [Strategy.Apple]: appleAuth,
+      [Strategy.Facebook]:facebookAuth,
+    }[strategy];
+
+    try {
+      const { createdSessionId, setActive} = await selectedAuth();
+      console.log(" ~ file: login.tsx:31 ~ onSelectAuth ~ createdSessionId", createdSessionId);
+
+      if (createdSessionId){
+        setActive!({session: createdSessionId})
+        router.back();
+      }
+    } catch (err) {
+      console.error('OAuth error:', err);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput 
+        autoCapitalize='none' 
+        placeholder='Email' 
+        style={[defaultStyles.inputField, { marginBottom: 30}]}
+      />
+   <TouchableOpacity style={defaultStyles.btn}>
+    <Text style={defaultStyles.btnText}>Continue</Text>
+   </TouchableOpacity>
+
+   <View style={styles.seperatorView}>
+     <View 
+       style={{
+         flex: 1,
+         borderBottomColor: '#000',
+         borderBottomWidth: StyleSheet.hairlineWidth,
+       }}
+     />
+     <Text style={styles.seperator}>Or</Text> 
+     <View 
+       style={{
+         flex: 1,
+         borderBottomColor: '#000',
+         borderBottomWidth: StyleSheet.hairlineWidth,
+       }}
+      /> 
+   </View>
+
+   <View style={{gap: 20}}>
+       <TouchableOpacity style={styles.btnOutline}>
+         <Ionicons name='call-outline' size={24} style={defaultStyles.btnIcon} />
+        <Text style={styles.btnOutlineText}>Continue with Phone</Text>
+       </TouchableOpacity>
+       <TouchableOpacity style={styles.btnOutline} onPress={() => onSelectAuth(Strategy.Apple)}>
+         <Ionicons name="logo-apple" size={24} style={defaultStyles.btnIcon} />
+        <Text style={styles.btnOutlineText}>Continue with Apple</Text>
+       </TouchableOpacity>
+       <TouchableOpacity style={styles.btnOutline}onPress={() => onSelectAuth(Strategy.Google)}>
+         <Ionicons name='logo-google' size={24} style={defaultStyles.btnIcon} />
+        <Text style={styles.btnOutlineText}>Continue with Google</Text>
+       </TouchableOpacity>
+       <TouchableOpacity style={styles.btnOutline}onPress={() => onSelectAuth(Strategy.Facebook)}>
+         <Ionicons name='logo-facebook' size={24} style={defaultStyles.btnIcon} />
+        <Text style={styles.btnOutlineText}>Continue with Facebook</Text>
+       </TouchableOpacity>
+   </View>
+  </View>
+ );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding:26,
+  },
+  seperatorView: {
+    flexDirection: 'row',
+    gap: 10,
+    alignItems: 'center',
+    marginVertical: 30,
+  },
+  seperator: {
+    fontFamily: 'mon-sb',
+    color: Colors.grey,
+  },
+  btnOutline: {
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: Colors.grey,
+    height: 50,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+  },
+  btnOutlineText: {
+    color: '#000',
+    fontSize: 16,
+    fontFamily: 'mon-sb',
+  }
+})
+export default Page;
---
+++ app/listing/[id].tsx
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native';
+import React from 'react';
+import { useLocalSearchParams } from 'expo-router';
+
+const Page = () => {
+  const { id } = useLocalSearchParams<{id: string}>();
+  console.log(' ~ file: [id].tsx ~ id:', id);  
+  return (
+    <View>
+      <Text>Page</Text>
+    </View>
+ );
+};
+
+export default Page;
